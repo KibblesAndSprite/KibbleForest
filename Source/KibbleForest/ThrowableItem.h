4c2a37ebd4750b8b0ddc2380ef797887a52ddc16
StevenMakeStuff <54723295+StevenMakeStuff@users.noreply.github.com>
2022-07-14 22:59:32 -0400

    Added in a few different scripts to start the item making process.

---
+++ Source/KibbleForest/ThrowableItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UsableItem.h"
+#include "ThrowableItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KIBBLEFOREST_API AThrowableItem : public AUsableItem
+{
+	GENERATED_BODY()
+	
+};
